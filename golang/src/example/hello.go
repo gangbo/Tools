16d5111a26b97dafb208ecb8ce1d7fa35eb2db69
gangbo <daigangbo@gmail.com>
2013-04-20 11:05:36 +0800

    #

---
+++ golang/src/example/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type S1 struct {
+    a int
+    b string
+}
+func main() {
+        var1 := S1{1,"2"}
+        fmt.Println("Hello, 世界")
+        fmt.Println(var1);
+}
